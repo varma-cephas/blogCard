0f3a53d23162e14f71119e14c944b40220bcbdc4
varmac <varmac231@gmail.com>
2024-01-10 21:51:52 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './main.css'
+
+function App() {
+  return (
+    <>  
+        <div className='cardContainer'>
+          <div className='infoContainer'>
+            <h4>Learning</h4>
+            <p>Published 21 Dec 2023</p>
+            <h2>HTML & CSS foundations</h2>
+            <p>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+            <h5>Greg Hooper</h5>
+          </div>
+        </div>
+
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="#">Your Name Here</a>.
+        </div>
+        
+    </>
+  )
+}
+
+export default App
